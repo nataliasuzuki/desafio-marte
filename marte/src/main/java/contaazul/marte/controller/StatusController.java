9dfd530f32a8bfe300def582d4489cb19203da2c
nataliasuzuki <61856025+nataliasuzuki@users.noreply.github.com>
2022-04-18 01:44:48 -0300

    Inclusão da lógica

---
+++ marte/src/main/java/contaazul/marte/controller/StatusController.java
@@ -0,0 +1,90 @@
+package contaazul.marte.controller;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class StatusController {
+	
+	@Autowired
+
+	@RequestMapping("/status")
+	public String check() {
+		return "Online";
+	}
+	
+	@PostMapping(path = "/rest/mars/{codigo}")
+	public String consultar(@PathVariable("codigo") String codigo) {
+		
+		if((!codigo.contains("L") && !codigo.contains("R") && !codigo.contains("M"))) {
+			return "erroCodigo";
+		}
+		
+		if(codigo.length() > 1) {
+			String[] comandoAux = codigo.split("(?<=[RL])");
+			String[] posicao = {"N", "E", "S", "W"};
+			int contadorPosicao = 0;
+		
+			System.out.println("inicio");
+			for(String x : comandoAux) {
+				System.out.println("nati: " + x);
+			}
+			System.out.println("fim");
+			System.out.println("tam: " + comandoAux.length);
+			
+			int posicaoX = 0;
+			int posicaoY = 0;
+			int posicaoMaxima = 5;
+			
+			for(String y : comandoAux) {
+				int contadorDuplicadoM = 0;
+				
+ 				for (int z = 0; z < y.length(); z++) {
+				    if (y.charAt(z) == 'M') {
+				    	contadorDuplicadoM++;
+				    }
+				}
+				if(contadorDuplicadoM == codigo.length()) {
+					return "erroDuplicado";
+				}
+				
+				if(y.charAt(y.length()-1) == 'R') {
+			    	posicaoX = posicaoX + contadorDuplicadoM;
+			    	if(posicaoY <= posicaoMaxima) {
+			    	List<String> list = Arrays.asList(posicao);
+			        Collections.rotate(list, -1);
+			        System.out.println("Rotated List: " + list);
+			        System.out.println(posicaoX + ", " + posicaoY + ", " + list.get(contadorPosicao));
+			    	}
+			    	else {
+			    		return "erroPosicao";
+			    	}
+			        
+			    }
+			    else if(y.charAt(y.length()-1) == 'L') {
+			    	posicaoY = posicaoY + contadorDuplicadoM;
+			    	if(posicaoY <= posicaoMaxima) {
+				    	List<String> list = Arrays.asList(posicao);
+				        Collections.rotate(list, 1);
+				        System.out.println("Rotated List: " + list);
+				        System.out.println(posicaoX + ", " + posicaoY + ", " + list.get(contadorPosicao));
+				    }
+			    	else {
+			    		return "erroPosicao";
+			    	}
+			        
+			    }
+			}
+		}
+		
+		return "";
+	}
+}
